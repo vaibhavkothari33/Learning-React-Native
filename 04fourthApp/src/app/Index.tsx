cd6a7d6172011ba26b3503c66e8de538e59ecfaf
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2024-10-13 20:13:33 +0530

    fourth app

---
+++ 04fourthApp/src/app/Index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Image from '../components/Image'
+const Index = () => {
+  return (
+    <View>
+      <Text>Index</Text>
+      <Image/>
+    </View>
+  )
+}
+
+export default Index
